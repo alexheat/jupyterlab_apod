ceee3de6fe7186e88f14e932ab4c213a75c9cf7d
Alex Heaton <aheaton@gmail.com>
2023-07-11 09:21:48 +0900

    git commit -m 'Add styling, attribution, error handling'

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 
 import { Widget } from '@lumino/widgets';
 
+
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+};
+
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
@@ -16,31 +26,79 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension.',
   requires: [ICommandPalette],
   autoStart: true,
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_apod is activated!');
-
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log(`JupyterLab extension jupyterlab_apod is activated! ${Date()}`);
 
     // Define a widget creator function,
     // then call it to make a new widget
-    const newWidget = () => {
+    const newWidget = async () => {
       // Create a blank content widget inside of a MainAreaWidget
       const content = new Widget();
+      content.addClass('my-apodWidget');
       const widget = new MainAreaWidget({ content });
-      widget.id = 'apod-jupyterlab';
-      widget.title.label = 'Astronomy Picture';
-      widget.title.closable = true;
+
+      // Add a div element to the content
+      let div = document.createElement('div');
+      content.node.appendChild(div);
+
+      // Add an image element to the content
+      let img = document.createElement('img');
+      content.node.appendChild(img);
+
+      let summary = document.createElement('p');
+      content.node.appendChild(summary);
+
+      // Get a random date string in YYYY-MM-DD format
+      function randomDate() {
+        const start = new Date(2010, 1, 1);
+        const end = new Date();
+        const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        return randomDate.toISOString().slice(0, 10);
+      }
+
+      // Save the random date to a variable
+      const randomDateString: string = randomDate();
+
+      div.innerText = randomDateString
+
+      // Fetch info about a random picture
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDateString}`);
+
+      if (!response.ok) {
+        const data = await response.json();
+        if (data.error) {
+          summary.innerText = data.error.message;
+        } else {
+          summary.innerText = response.statusText;
+        }
+      } else {
+        const data = await response.json() as APODResponse;
+
+        if (data.media_type === 'image') {
+          // Populate the image
+          img.src = data.url;
+          img.title = data.title;
+          summary.innerText = data.title;
+          if (data.copyright) {
+            summary.innerText += ` (Copyright ${data.copyright})`;
+          }
+        } else {
+          summary.innerText = 'Random APOD fetched was not an image.';
+        }
+      }
       return widget;
     }
-    let widget = newWidget();
+
+    let widget = await newWidget();
 
     // Add an application command
     const command: string = 'apod:open';
     app.commands.addCommand(command, {
       label: 'Random Astronomy Picture',
-      execute: () => {
+      execute: async () => {
         // Regenerate the widget if disposed
         if (widget.isDisposed) {
-          widget = newWidget();
+          widget = await newWidget();
         }
         if (!widget.isAttached) {
           // Attach the widget to the main work area if it's not there
